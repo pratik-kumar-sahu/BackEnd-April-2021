30be822bf43466aaf5d3a079f0f68bdea7a070d8
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-25 10:26:06 +0530

    added fs, ejs module and updated code to save form data to local json file

--- Q-1/app.js
+++ Q-1/app.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const ejs = require("ejs");
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+const usersData = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/usersData.json`)
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("signupForm");
+});
+
+app.get("/usersData", (req, res) => {
+  res.json(usersData);
+});
+
+app.post("/userSignup", (req, res) => {
+  newUser = Object.assign(req.body);
+  usersData.push(newUser);
+
+  fs.writeFile(
+    `${__dirname}/data/usersData.json`,
+    JSON.stringify(usersData),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          user: newUser,
+        },
+      });
+    }
+  );
+
+  res.redirect("/usersData");
 });
 
 const PORT = 4000;
