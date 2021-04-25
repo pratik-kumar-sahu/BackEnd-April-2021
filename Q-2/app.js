b03b0c38b692a66a0c16a0272bf027a8896a512a
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-25 12:18:03 +0530

    Updated app.js to store uploaded file using multer and fs modules

--- Q-2/app.js
+++ Q-2/app.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const ejs = require("ejs");
+const multer = require("multer");
+const fs = require("fs");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+const upload = multer({
+  dest: "/uploads",
+});
+
 app.get("/", (req, res) => {
-  res.send("Hey there");
+  res.render("form");
+});
+
+app.get("/image", (req, res) => {
+  res.sendFile(`${__dirname}/uploads/image.png`);
+});
+
+app.post("/uploadImage", upload.single("file"), (req, res) => {
+  // console.log(req.file);
+  const filePath = req.file.path;
+  const uploadPath = `${__dirname}/uploads/image.png`;
+
+  const allowedType = ["image/png", "image/jpeg", "image/gif"];
+
+  if (allowedType.includes(req.file.mimetype)) {
+    fs.rename(filePath, uploadPath, (err) => {
+      if (err) return;
+      res.status(200).redirect("/image");
+    });
+  } else {
+    fs.unlink(filePath, (err) => {
+      if (err) return;
+      res.send("Only png, jpeg and gif files are allowed");
+    });
+  }
 });
 
 PORT = 4001;
